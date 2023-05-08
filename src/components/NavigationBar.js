b434ada3aba6335aa1f3725961d0853e4b79fc2e
Asia K <asialakaygrady@gmail.com>
2023-05-08 10:18:06 -0400

    menu responsiveness

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,127 +3,81 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import styles from '../styles/NavigationBar.module.css';
-
+import { Navbar, Nav, Button, Offcanvas, Container } from 'react-bootstrap';
 
 const NavigationBar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
+  const router = useRouter();
+  const handleClick = (event) => {
+    event.preventDefault();
+    router.push('/new-repair-request');
+  };
 
-    const router = useRouter();
-    const handleClick = (event) => {
-        event.preventDefault();
-        router.push('/new-repair-request');
-      };
-      
-      const toggleMenu = () => {
-        setShowMenu(!showMenu);
-      };
-
-
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
-    <nav 
-    className={
-      styles['navbar']}>
-
-      <div 
-      className={
-        styles['navbar-brand']}
+    <Navbar bg="light" variant="light" className={styles.navbar}>
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <Image
+            src="/repair-icon.png"
+            alt="Repair Tracker Pro"
+            width={30}
+            height={30}
+            className="d-inline-block align-top"
+          />{' '}
+          Repair Tracker Pro
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} href="/" passHref>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} href="/repair-list" passHref>
+              Repairs
+            </Nav.Link>
+            <Nav.Link as={Link} href="/new-repair-request" passHref>
+              New Request
+            </Nav.Link>
+            <Nav.Link as={Link} href="/login" passHref>
+              Login
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Button variant="outline-primary" onClick={() => setShowMenu(true)}>
+          Menu
+        </Button>
+        <Offcanvas
+          show={showMenu}
+          onHide={() => setShowMenu(false)}
+          placement="end"
         >
-        <Image 
-        src="/repair-icon.png" 
-        alt="Repair Tracker Pro" 
-        width={30} height={30} 
-        />
-   {/*      <Link 
-        href="/" 
-        className={
-          styles['navbar-brand-link']}
-          >Repair Tracker Pro
-        </Link> */}
-      </div>
-
-
-      <div className={
-        styles['navbar-links']}>
-
-{/* navbar-link = not for small screen */}
-   {/*      <Link 
-        href="/" 
-        className={
-          styles['navbar-link']}
-          >Home
-        </Link> */}
-
-    {/*     <Link 
-        href="/repair-list" 
-        className={styles['navbar-link']}
-        >Repairs
-        </Link> */}
-
-  {/*       <Link 
-        href="/new-repair-request"
-        className={styles['navbar-link']}
-        >New Request
-        </Link> */}
-
- {/*        <Link 
-        href="/login" 
-        className={styles['navbar-link']}
-        >Login
-        </Link> */}
-
-
-
-
-    {/*     <button 
-        className={styles['hamburger-icon']} 
-        onClick={toggleMenu}>
-          <FaBars />
-        </button> */}
-      </div>
-
-
-
-
-
-
-      <div 
-      className={showMenu 
-      ? styles['mobile-menu'] 
-      : styles['mobile-menu-hidden']}
-      >
-
-{/*       <Link 
-      href="/" 
-      className={
-        styles['mobile-menu-link']}
-        >
-Home
-        </Link> */}
-        <Link 
-        href="/repair-list" 
-        className={
-        styles['mobile-menu-link']}          >
-Repairs
-        </Link>
-{/*         <Link 
-        href="/new-repair-request" 
-        className={
-          styles['mobile-menu-link']}
-          >
-New Request
-        </Link> */}
-{/*         <Link 
-        href="/login" 
-        className={
-          styles['mobile-menu-link']}
-          >
-Login
-        </Link> */}
-      </div>
-
-    </nav>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="flex-column">
+              <Nav.Link as={Link} href="/" passHref>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} href="/repair-list" passHref>
+                Repairs
+              </Nav.Link>
+              <Nav.Link as={Link} href="/new-repair-request" passHref>
+                New Request
+              </Nav.Link>
+              <Nav.Link as={Link} href="/login" passHref>
+                Login
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
